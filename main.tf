0051bd1a4c5e73890c07de3d39989911168a58ce
Karthik <33841263+codeincrypt@users.noreply.github.com>
2024-10-18 18:55:00 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,12 @@
+# Specify the provider
+provider "aws" {
+  region = "us-east-1" # Change this to your preferred region
+}
+
+# Fetch all S3 buckets
+data "aws_s3_buckets" "all_buckets" {}
+
+# Output the bucket names
+output "bucket_names" {
+  value = data.aws_s3_buckets.all_buckets.ids
+}
