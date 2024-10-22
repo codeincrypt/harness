5235a3cec8cf06cf636ac76f5e4ed2de2a94560b
Karthik <33841263+codeincrypt@users.noreply.github.com>
2024-10-22 15:36:25 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,13 +1,9 @@
 provider "aws" {
-  region = "us-east-1"  # Change this to your preferred region
+  region = "us-east-1"  # Change to your preferred region
 }
 
-resource "null_resource" "list_buckets" {
-  provisioner "local-exec" {
-    command = "aws s3api list-buckets --query 'Buckets[*].Name' --output json"
-  }
-}
+data "aws_s3_buckets" "all_buckets" {}
 
 output "bucket_names" {
-  value = null_resource.list_buckets.id
+  value = data.aws_s3_buckets.all_buckets.ids
 }
